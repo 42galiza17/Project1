c8a1044845c58f4e7348fbdff74e11137405ea4a
Reynier John Mercene Portugaliza <61039158+42galiza17@users.noreply.github.com>
2021-05-14 07:57:49 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+mybutton = document.getElementById("myBtn");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0; 
+}
+
+// menu
+
+const menu = document.querySelector('.menu');
+const nav = document.querySelector('.nav-links');
+const navLink = document.querySelectorAll('.nav-links li');
+
+function menuBar() {
+    menu.addEventListener('click', () => {
+        menu.classList.toggle('change');
+    if (nav.style.display){
+            nav.style.display = ""
+        }
+        else {
+            nav.style.display = "flex"
+        }
+    })
+
+    
+
+}
+
+menuBar();
+
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
